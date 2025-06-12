27c9dbaea0b1dab452955d9ef11bd7a05e3a25d7
Mansha7 <91420957+Mansha7@users.noreply.github.com>
2025-06-12 11:48:40 +0530

    Add files via upload

---
+++ cost_optimiser.py
@@ -0,0 +1,43 @@
+from utils.ollama_client import call_ollama
+
+async def get_cost_model(information: str):
+    template = f"""
+<|start_of_role|>system<|end_of_role|>  
+You are an expert in cloud infrastructure cost optimization. You are cautious, ethical, and technically precise.  
+<|end_of_text|>  
+<|start_of_role|>user<|end_of_role|>
+Given the architectural specifications of a deployment, like the RAM, list of services, data centre locations etc., I want you to:
+1) Analyse the cost estimation of the application, determine if it's cost-friendly or not.
+2) Recommend ways to minimise the cost by analysing what operations can be cut down without affecting performance and sustainability.
+3) For each recommendation:
+    Provide specific IBM Cloud service-level changes or configurations if applicable.
+    Identify one recommendation as the "best option" overall in terms of cost vs. effort required.
+
+Important Instructions:
+- Output ONLY the recommendations and the source links or docuemnts where you referenced this data from. Do not dive into the estimation technique and thought process much, output the recommendation directly.
+- Privacy: Exclude any personally identifiable information (PII) such as names, email addresses, phone numbers, device IDs, or system locations.
+- Ensure your response is concise, accurate, and unbiased, maintaining a professional and respectful tone.
+- Answer only using the information present in the input. Do not fabricate details or make assumptions.
+- Focus strictly on technical content; omit any greetings, emotions, or conversational elements.
+- Consolidate related information where applicable.
+- Acronyms: Do not expand acronyms.
+- Output: Do not repeat instructions. If no information is available for a topic, do not mention its unavailability. Do not include the specified summary statements.
+
+Response Format:
+        Recommendation 1: [Title]
+        Justification: [Why this helps]
+        Estimated Impact: [Quantified or rough estimate]
+        IBM Cloud-Specific Steps: [What to do]
+        (Repeat for up to 4)
+
+Application Details:
+{information}
+
+<|end_of_text|>
+<|start_of_role|>assistant<|end_of_role|>
+"""
+
+    response = await call_ollama(template)
+    print(f"Response from Cost: {response}")
+
+    return response
